dd7a5961494fb940a80cefc041c36d5463f8e3fc
Cleberton Kobay <Clebertonkobay@gmail.com>
2023-05-24 18:27:44 -0300

    Add do model jogador e pagamento

--- src/main/java/JF/pagamentos/Model/Jogador.java
+++ src/main/java/JF/pagamentos/Model/Jogador.java
@@ -8,7 +8,53 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(a)
+@Table(name = "jogador")
 public class Jogador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long cod_jogador;
+
+    @Column
+    private String nome;
+    
+    @Column
+    private String email;
+
+    @Column
+    private String datanasc;
+
+
+    public long getCod_jogador() {
+        return this.cod_jogador;
+    }
+
+    public void setCod_jogador(long cod_jogador) {
+        this.cod_jogador = cod_jogador;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    };
+
+    public String getDatanasc() {
+        return this.datanasc;
+    }
+
+    public void setDatanasc(String datanasc) {
+        this.datanasc = datanasc;
+    }
+
     
 }
--- src/main/java/JF/pagamentos/Model/Pagamento.java
+++ src/main/java/JF/pagamentos/Model/Pagamento.java
@@ -1,5 +1,77 @@
 package JF.pagamentos.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pagamento")
 public class Pagamento {
-    
+    @Id
+    @GeneratedValue(strategy = GeneratedValue.AUTO)
+    private long cod_pagamento;
+
+    @Column
+    private short ano;
+
+    @Column
+    private short mes;
+
+    @Column
+    private double valor;
+
+    @Column
+    private String cod_jogador;
+
+    public long getCod_pagamento() {
+        return this.cod_pagamento;
+    }
+
+    public void setCod_pagamento(long cod_pagamento) {
+        this.cod_pagamento = cod_pagamento;
+    }
+
+    public Object get() {
+		return this.;
+	}
+
+    public void set(Object ) {
+		this. = ;
+	}
+
+    public short getAno() {
+        return this.ano;
+    }
+
+    public void setAno(short ano) {
+        this.ano = ano;
+    }
+
+    public short getMes() {
+        return this.mes;
+    }
+
+    public void setMes(short mes) {
+        this.mes = mes;
+    }
+
+    public double getValor() {
+        return this.valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getCod_jogador() {
+        return this.cod_jogador;
+    }
+
+    public void setCod_jogador(String cod_jogador) {
+        this.cod_jogador = cod_jogador;
+    }
+
 }
